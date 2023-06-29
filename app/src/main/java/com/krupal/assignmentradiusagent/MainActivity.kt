43a34ab6369f889897df7673e8fbda3904cd39c4
Krupal Bhardiya <krbhardiya@gmail.com>
2023-06-29 20:19:46 +0530

    MainActivity with some basics added

--- app/src/main/java/com/krupal/assignmentradiusagent/MainActivity.kt
+++ app/src/main/java/com/krupal/assignmentradiusagent/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.krupal.assignmentradiusagent
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.Constraints
+import androidx.work.ExistingWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.google.android.material.snackbar.Snackbar
+import com.krupal.assignmentradiusagent.databinding.ActivityMainBinding
+import com.krupal.assignmentradiusagent.service.FacilitiesDownloadService
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var snackbar: Snackbar? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_reload -> {
+                generateRequest()
+                return true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun generateRequest() {
+        val request = OneTimeWorkRequest.Builder(FacilitiesDownloadService::class.java)
+            .setConstraints(
+                Constraints.Builder()
+                    .setRequiredNetworkType(NetworkType.CONNECTED)
+                    .build()
+            )
+            .build()
+        WorkManager.getInstance(this)
+            .enqueueUniqueWork("request", ExistingWorkPolicy.REPLACE, request)
     }
 }
